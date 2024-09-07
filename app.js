cf974af09d004765a1588bbfa9f9ba4b634970ba
A-KuGo <Ikuggo@gmail.com>
2024-09-07 19:20:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const button = document.querySelector('button');
 const output = document.querySelector('.output');
 
 button.addEventListener('click', function(){
-    output.innerHTML = 'Мама, привет ♥';
+    output.innerHTML = 'Воин, привет ♥';
 });
